5d80ff37592085ddff1fb501d9288bba7410d40f
Firefly4266 <gaines.johnp@gmail.com>
2017-08-27 23:41:09 -0700

    exercise complete.

--- InitialAccount/Account.cs
+++ InitialAccount/Account.cs
@@ -5,7 +5,27 @@ public class Account
     private int id;
     private decimal balance;
     private string owner;
-    private int numXact;
+    private int numXact = 0;
+    public Account(decimal balance, string owner, int id)
+        : this(balance, owner)
+    {
+        Console.WriteLine("Account (decimal, string, int)");
+        this.id = id;
+    }
+    public Account(decimal balance, string owner)
+        : this(balance)
+    {
+        Console.WriteLine("Account(decimal, string)");
+        this.owner = owner;
+    }
+    public Account(decimal balance)
+    {
+        Console.WriteLine("Account(decimal)");
+        this.balance = balance ;
+    }
+    public Account()
+    {
+    }
     public string GetStatement()
     {
         string s = "Owner = " + owner + ", Id = " + id + ", Transactions = " + numXact + ", Balance = " + balance;
